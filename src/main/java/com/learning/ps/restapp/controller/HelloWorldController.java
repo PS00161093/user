6535057f3377749f466007a838e0dfca4122d33d
Prashant Sharma <prashant.sharma1thoughtworks.com>
2022-01-01 14:54:20 +0530

    Added i18n implementation

--- src/main/java/com/learning/ps/restapp/controller/HelloWorldController.java
+++ src/main/java/com/learning/ps/restapp/controller/HelloWorldController.java
@@ -1,12 +1,21 @@
 package com.learning.ps.restapp.controller;
 
 import com.learning.ps.restapp.model.HelloWorldBean;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Locale;
 
 @RestController
 public class HelloWorldController {
+
+    @Autowired
+    private MessageSource messageSource;
 
     @GetMapping(path = "/hello-world")
     public String helloWorld() {
@@ -21,5 +30,10 @@ public class HelloWorldController {
     @GetMapping(path = "/hello-world-path-variable/{name}")
     public HelloWorldBean helloWorld(@PathVariable String name) {
         return new HelloWorldBean(String.format("Hello, %s!", name));
+    }
+
+    @GetMapping(path = "/hello-world-i18n")
+    public String helloWorldI18n() {
+        return messageSource.getMessage("good.morning.message", null, LocaleContextHolder.getLocale());
     }
 }
