c762d3abfd2ee694c9ea1071d2bc73990904b740
Prashant Sharma <prashant.sharma1thoughtworks.com>
2022-01-02 12:50:12 +0530

    Added JPA capability

---
+++ src/main/java/com/learning/ps/restapp/dao/UserDummyRepository.java
@@ -0,0 +1,45 @@
+package com.learning.ps.restapp.dao;
+
+import com.learning.ps.restapp.exception.UserNotFoundException;
+import com.learning.ps.restapp.model.User;
+import org.springframework.stereotype.Component;
+
+import java.util.*;
+
+@Component
+public class UserDummyRepository {
+
+    private static List<User> users = new ArrayList<>();
+
+    private static int userCount = 3;
+
+    static {
+        users.add(new User(1, "Joey", new Date()));
+        users.add(new User(3, "Ross", new Date()));
+        users.add(new User(2, "Phoebe", new Date()));
+    }
+
+    public List<User> findAll() {
+        return users;
+    }
+
+    public User saveUser(User newUser) {
+        if (Objects.isNull(newUser.getId())) newUser.setId(++userCount);
+        users.add(newUser);
+        return newUser;
+    }
+
+    public User findOne(int id) {
+        return users.stream().filter(user -> user.getId() == id).findFirst().orElse(null);
+    }
+
+    public User deleteById(int id) {
+        User deleteUser = users.stream()
+                .filter(user -> user.getId() == id)
+                .findFirst()
+                .orElseThrow(() -> new UserNotFoundException(String.format("User not found with id = %d", id)));
+
+        users.remove(deleteUser);
+        return deleteUser;
+    }
+}
--- src/main/java/com/learning/ps/restapp/dao/UserRepository.java
+++ src/main/java/com/learning/ps/restapp/dao/UserRepository.java
@@ -1,45 +1,9 @@
 package com.learning.ps.restapp.dao;
 
-import com.learning.ps.restapp.exception.UserNotFoundException;
-import com.learning.ps.restapp.model.User;
-import org.springframework.stereotype.Component;
+import com.learning.ps.restapp.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-import java.util.*;
-
-@Component
-public class UserRepository {
-
-    private static List<User> users = new ArrayList<>();
-
-    private static int userCount = 3;
-
-    static {
-        users.add(new User(1, "Joey", new Date()));
-        users.add(new User(3, "Ross", new Date()));
-        users.add(new User(2, "Phoebe", new Date()));
-    }
-
-    public List<User> findAll() {
-        return users;
-    }
-
-    public User saveUser(User newUser) {
-        if (Objects.isNull(newUser.getId())) newUser.setId(++userCount);
-        users.add(newUser);
-        return newUser;
-    }
-
-    public User findOne(int id) {
-        return users.stream().filter(user -> user.getId() == id).findFirst().orElse(null);
-    }
-
-    public User deleteById(int id) {
-        User deleteUser = users.stream()
-                .filter(user -> user.getId() == id)
-                .findFirst()
-                .orElseThrow(() -> new UserNotFoundException(String.format("User not found with id = %d", id)));
-
-        users.remove(deleteUser);
-        return deleteUser;
-    }
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
 }
